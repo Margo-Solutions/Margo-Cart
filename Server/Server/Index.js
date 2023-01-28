58c2ab4f7021ea44f4240246599fd56de8dec864
Martin Kopland <Martinkopland@gmail.com>
2023-01-28 14:47:51 +0100

    la til fungerende client app

--- Server/Server/Index.js
+++ Server/Server/Index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
-
+app.use(express.static('public'));
+let PORT =process.env.PORT || 5000;
+//const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -18,9 +20,9 @@ app.post('/handleliste', async (req, res) => {
             [handleliste]
         );
         res.json(newHandleliste.rows[0]);
-        } catch (err) {
+        } catch (err) {3
         console.error(err.message);
-    }
+    }   
 });
 
 // get all handleliste //
@@ -70,8 +72,7 @@ app.delete('/handleliste/:id', async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
-
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+    }
+);
