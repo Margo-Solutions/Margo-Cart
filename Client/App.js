e3fc0c4a50e6a1ffcf095a17e5c50aa3a9254a23
Martin Kopland <Martinkopland@gmail.com>
2023-01-28 15:36:53 +0100

    client

---
+++ Client/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+import { useState, useEffect } from 'react';
+//components
+
+
+export default function App() {
+  const [handleliste, sethandleliste] = useState('');
+  const 
+  onsubmitform = async(e) => {
+    e.preventDefault();
+    try {
+        const body = { handleliste };
+        const response = await fetch("http://localhost:5000/handleliste", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+       // console.log(response);  
+      }
+        catch (err) {
+                console.error(err.message);
+            }
+        }
+
+    return ( 
+    <View style={styles.container}>
+        <View style={styles.inputView}>
+        <TextInput
+        style={{height: 40}}
+        placeholder="Type here to insert data!"
+        let food = "ost"
+        value={handleliste}
+        onChangeText={newText => sethandleliste(newText)}
+    
+      />
+      </View>
+      <Button title="add" onPress={onsubmitform}>  </Button>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputView : {
+    backgroundColor : "#8FD6F2DB",
+    borderRadius : 30,
+    borderColor : "black",
+    borderWidth : 1,
+    width : "70%",
+    height : 45,
+    marginBottom : 20,
+    alignItems : "center"
+  },
+
+});
---
+++ Client/compoents/input.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+
+export default function Input() {
+    const [text, setText] = useState('');
+    const onSubmit = async(e) => {
+        try {
+        e.preventDefault();
+        const body = { text };
+        const response = await fetch("http://localhost:5000/handleliste", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+        }
+        catch (err) {
+                console.error(err.message);
+            }
+        }
+
+    return ( 
+    <View>
+        <TextInput style ={placeholder = "type input here"}
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+        /> 
+      <Text>Input</Text>
+      <Button title="add" onPress={setText}>  </Button>
+    </View>
+  );
+}
