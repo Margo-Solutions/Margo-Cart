d902681852d96addeab77d3b576d1fbe06f6b0c3
TrudeJohansen <113107099+TrudeJohansen@users.noreply.github.com>
2023-04-13 10:03:56 +0200

    lagt til increment og decrement på handleister

--- Client/Screens/Handleliste.js
+++ Client/Screens/Handleliste.js
@@ -2,100 +2,138 @@ import * as React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import { StyleSheet, TextInput, View, Image, Button, Text, TouchableOpacity, FlatList } from 'react-native';
 import { handContext } from '../context/listeHandler';
+import { number } from 'yup';
 
 
-export default function Handleliste({navigation, route}) {
-    const {handlelisteID} = route.params; // getting handleliste id from previous screen
-    const {handleliste_tittel, getHandlelisteName, ListVarerHandleliste, handleliste, updateHandleliste } = useContext(handContext);
-    
-    const updateOrRemove = async (id, antall) => { // update or remove item from handleliste
-        let Numb = antall - 1;
-        if (antall == 1) {
-            removeItem(id);
-        }
-            else {
-                updateHandleliste(Numb, id, handlelisteID);
-            }
-        ListVarerHandleliste(handlelisteID);
-        };
+export default function Handleliste({ navigation, route, props }) {
+  const { handlelisteID } = route.params; // getting handleliste id from previous screen
+  const { handleliste_tittel, getHandlelisteName, ListVarerHandleliste, handleliste, updateHandleliste } = useContext(handContext);
+  const [value, setValue] = useState();
+  const maximum = 100;
 
-    const removeItem = async (id) => { // remove item from handleliste
-        try {
-            const body = { id };
-            const response = await fetch(`http://10.0.2.2:5000/margodatabase/handleliste/remove/`, {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body)
-            });
-            const parseRes = await response.json();
-            ListVarerHandleliste(handlelisteID);
-            
-        }catch (err) {
-                console.error(err.message);
-        }
-        };
 
-        const pressHandler = (handleliste_id) =>{ // press handler to handle item clicks  
-          navigation.navigate('Varer',{
-              handlelisteID: handleliste_id
-            });
-          };
+  const increment = async (id, antall) => {
+    const { max } = maximum;
+    if (typeof max === "number" && antall >= max) return;
 
-    useEffect(() => { // use effect to refresh handleliste and items
-        getHandlelisteName(handlelisteID);
-        ListVarerHandleliste(handlelisteID);
-    }, []);
-    return (
-      <View style={styles.container}>
+    const newValue = antall + 1;
+    //updateHandleliste(newValue, id, handlelisteID);
+    setValue(newValue);
+    updateHandleliste(newValue, id, handlelisteID);
+    ListVarerHandleliste(handlelisteID);
+  };
+
+  const decrement = async (id, antall) => {
+    const newValue = antall - 1;
+    if (newValue <= 0) {
+      removeItem(id);
+    } else {
+      //updateHandleliste(newValue, id, handlelisteID);
+      setValue(newValue);
+      updateHandleliste(newValue, id, handlelisteID);
+    }
+    ListVarerHandleliste(handlelisteID);
+  };
+
+
+  const removeItem = async (id) => { // remove item from handleliste
+    try {
+      const body = { id };
+      const response = await fetch(`http://10.0.2.2:5000/margodatabase/handleliste/remove/`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+      const parseRes = await response.json();
+      ListVarerHandleliste(handlelisteID);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const pressHandler = (handleliste_id) => { // press handler to handle item clicks  
+    navigation.navigate('Varer', {
+      handlelisteID: handleliste_id
+    });
+  };
+
+  useEffect(() => { // use effect to refresh handleliste and items
+    getHandlelisteName(handlelisteID);
+    ListVarerHandleliste(handlelisteID);
+  }, []);
+
+  useEffect(() => { // use effect to refresh handleliste and items
+    getHandlelisteName(handlelisteID);
+    ListVarerHandleliste(handlelisteID);
+  }, [value]);
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/logoM.png')}
+        />
+      </View>
+      <View style={styles.line} />
+      <View style={styles.titleContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder=""
+          value={handleliste_tittel}
+        />
+        <Button title="Lagre endringer" />
+      </View>
+      <View style={styles.listContainer}>
         <View>
-          <Image
-            style={styles.image}
-            source={require('../assets/images/logoM.png')}
-             />
-          </View>
-          <View style={styles.line} />
-              <View style={styles.titleContainer}>
-              <TextInput 
-                  style={styles.textInput}
-                  placeholder=""
-                  value={handleliste_tittel}
-                  />
-                  <Button title = "Lagre endringer" />
-              </View>
-              <View style={styles.listContainer}>
-              <View>
-                <TouchableOpacity style={styles.varerButtonStyle} onPress={() => pressHandler(handlelisteID) }>
-                  <Text style={styles.opacityText}>Legg til varer</Text>
-                </TouchableOpacity>
-              </View>
-              <FlatList data={handleliste} renderItem={(itemData) => {
-              return(
-                <TouchableOpacity onPress={()=> updateOrRemove(itemData.item.id, itemData.item.antall)} >
-                <View style={styles.listItem}>
-                  <Text style={styles.listText}>{itemData.item.antall}x {itemData.item.vare_navn}</Text>
-                  <Text style={styles.lineText}>___________________________________________</Text>
-                </View>
-                </TouchableOpacity>
-              );
-              
-            }}
-              alwaysBounceVertical={false}
-             />
-             <View style={styles.buttonStyleNavigation}> 
-                <Button
-                    title="Naviger"
-                    color = "#8FD6F2"
-                    onPress={()=> navigation.navigate("Finn Butikk")}/>
+          <TouchableOpacity style={styles.varerButtonStyle} onPress={() => pressHandler(handlelisteID)}>
+            <Text style={styles.opacityText}>Legg til varer</Text>
+          </TouchableOpacity>
+        </View>
+        <FlatList data={handleliste} renderItem={(itemData) => {
+          return (
+            <View style={styles.listContainer}>
+              <View style={styles.listItem}>
+                <Text style={styles.listText}>{itemData.item.vare_navn}</Text>
+                <View style={[styles.inputContainer]}>
+                  <TouchableOpacity style={styles.button} onPress={() => { decrement(itemData.item.id, itemData.item.antall) }}>
+                    <Text style={styles.buttonText}>-</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.value}>{itemData.item.antall}</Text>
+                  <TouchableOpacity style={styles.button} onPress={() => { increment(itemData.item.id, itemData.item.antall) }}>
+                    <Text style={styles.buttonText}>+</Text>
+                  </TouchableOpacity>
                 </View>
               </View>
-              </View>
-      );
+              <Text style={styles.lineText}>___________________________________________</Text>
+            </View>
+          );
+
+        }}
+          alwaysBounceVertical={false}
+        />
+        <View style={styles.buttonStyleNavigation}>
+          <Button
+            title="Naviger"
+            color="#8FD6F2"
+            onPress={() => navigation.navigate("Finn Butikk")} />
+        </View>
+      </View>
+    </View >
+  );
 }
 
 const styles = StyleSheet.create({
   container: { // top bar container
     flex: 1,
     backgroundColor: '#66A2BA',
+  },
+  listeContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: { // image styling 
     width: 130,
@@ -117,7 +155,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
     backgroundColor: "#CADCFF",
-    justifyContent:'center',
+    justifyContent: 'center',
   },
   titleStyle: {  // text style for handleliste
     textAlign: 'center',
@@ -130,39 +168,63 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   listItem: { // view style for the text inside flatlist
+    flexDirection: 'row',
+    justifyContent: 'center',
     margin: 8,
     padding: 6,
     borderRadius: 10,
     backgroundColor: 'transparent',
   },
   varerButtonStyle: { // button for varer style
-      backgroundColor: 'transparent',
-    },
-    opacityText: { // text for add items 
-      color: '#646161',
-      textAlign: 'center',
-      fontSize: 18,
-    },
-    textInput: { // text input style for input title container
-      width: 350,
-      height: 35,
-      borderColor: '#e4d0ff',
-      backgroundColor: '#CADCFF',
-      color: '#120438',
-      borderWidth: 1,
-      borderRadius: 6,
-      textAlign: 'center',
-      fontSize: 20,
-    },
-    lineText: { // line style for lines in flatlist
-      textAlign: 'center',
-    },
-    buttonStyleNavigation: {
-      height: 35,
-      width: 255, 
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: 10,
-      marginLeft:80,
-    },
+    backgroundColor: 'transparent',
+  },
+  opacityText: { // text for add items 
+    color: '#646161',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  textInput: { // text input style for input title container
+    width: 350,
+    height: 35,
+    borderColor: '#e4d0ff',
+    backgroundColor: '#CADCFF',
+    color: '#120438',
+    borderWidth: 1,
+    borderRadius: 6,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  lineText: { // line style for lines in flatlist
+    textAlign: 'center',
+  },
+  buttonStyleNavigation: {
+    height: 35,
+    width: 255,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+    marginLeft: 80,
+  },
+  inputContainer: {
+    //backgroundColor: '#66A2BA',
+    //borderRadius: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    right: 50,
+    position: 'absolute'
+  },
+  button: {
+    padding: 6
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: "black",
+    fontSize: 20,
+  },
+  value: {
+    fontWeight: 'bold',
+    color: "black",
+    fontSize: 20,
+    marginHorizontal: 4,
+  },
 });
